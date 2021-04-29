e61c3758dfb1a87b258b491cd1a18f2d9668577a
Julian Caceres <caceresf.julian@gmail.com>
2021-04-29 13:53:00 -0500

    Add image input, processing and validation

--- frontend/src/components/auth/Registro.js
+++ frontend/src/components/auth/Registro.js
@@ -7,10 +7,38 @@ function Registro() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordVerify, setPasswordVerify] = useState("");
+    const [file, setFile] = useState("");
+    const [imagePreview, setImagePreview] = useState("");
+    const [base64, setBase64] = useState("");
     //definimos los datos que serán capturados para ser enviados al back.
 
-    const {obtenerLoggeo} = useContext(AuthContext);
+    const { obtenerLoggeo } = useContext(AuthContext);
     const historial = useHistory();
+
+    // Función para interpretar la imagen recién subida, mostrarla en pantalla y almacenarla en base64
+    function photoUpload(file) {
+        const reader = new FileReader();
+        console.log("reader", reader)
+        console.log("file", file)
+        if (reader !== undefined && file !== undefined) {
+            if (file.size > 2097152) {
+                alert('Verifique el archivo e intente de nuevo.');
+                setFile("");
+            } else if (file.type !== "image/png") {
+                alert('Verifique la extensión del archivo e intente de nuevo.');
+                setFile("");
+            }
+            else {
+                reader.onloadend = (e) => {
+                    setFile(file);
+                    setImagePreview(reader.result);
+                    setBase64(btoa(e.target.result));
+                }
+            }
+        }
+    }
+
+
 
     //Definimos una función ASÍNCRONA para el onSubmit del form
     async function registrar(e) {
@@ -22,7 +50,8 @@ function Registro() {
             const datosRegistrados = {
                 email,
                 password,
-                passwordVerify
+                passwordVerify,
+                base64
             };
 
             await axios.post("http://localhost:4000/auth", datosRegistrados);
@@ -36,35 +65,79 @@ function Registro() {
         }
     }
 
+    if (imagePreview) {
+        //Creamos una encuesta común.
+        return (
+            //Añadimos a cada input el valor y hook asociado por onChange.
+            <div>
+                <h1>Registrar nueva cuenta</h1>
+                <form onSubmit={registrar} encType="multipart/form-data">
+                    <input
+                        type="email"
+                        placeholder="Correo electrónico"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Verifica tu contraseña"
+                        value={passwordVerify}
+                        onChange={(e) => setPasswordVerify(e.target.value)}
+                    />
+                    <input
+                        type="file"
+                        accept=".png"
+                        onChange={(e) => photoUpload(e.target.files[0])}
+                    />
+                    <button type="submit">Crear cuenta</button>
+                </form>
+                <img src={imagePreview} alt="Upload" width="150" height="150" />
+            </div>
+        )
+    } else {
+        //Creamos una encuesta común.
+        return (
+            //Añadimos a cada input el valor y hook asociado por onChange.
+            <div>
+                <h1>Registrar nueva cuenta</h1>
+                <form onSubmit={registrar} encType="multipart/form-data">
+                    <input
+                        type="email"
+                        placeholder="Correo electrónico"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Verifica tu contraseña"
+                        value={passwordVerify}
+                        onChange={(e) => setPasswordVerify(e.target.value)}
+                    />
+                    <input
+                        type="file"
+                        accept=".png, .jpg"
+                        onChange={(e) => photoUpload(e.target.files[0])}
+                    />
+                    <button type="submit">Crear cuenta</button>
+                </form>
+            </div>
+        )
+    }
 
-    //Creamos una encuesta común.
-    return (
-        //Añadimos a cada input el valor y hook asociado por onChange.
-        <div>
-            <h1>Registrar nueva cuenta</h1>
-            <form onSubmit={registrar}>
-                <input
-                    type="email"
-                    placeholder="Correo electrónico"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Contraseña"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Verifica tu contraseña"
-                    value={passwordVerify}
-                    onChange={(e) => setPasswordVerify(e.target.value)}
-                />
-                <button type="submit">Crear cuenta</button>
-            </form>
-        </div>
-    )
+
+
 }
 
 export default Registro
