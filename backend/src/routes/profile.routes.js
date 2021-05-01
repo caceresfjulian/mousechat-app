f4de7a37455bfe0be4eecdcf88f6a924701e52f5
Julian Caceres <caceresf.julian@gmail.com>
2021-05-01 13:31:42 -0500

    Add update myProfile route

--- backend/src/routes/profile.routes.js
+++ backend/src/routes/profile.routes.js
@@ -9,9 +9,27 @@ router.get('/', auth, async (req, res) => {
     try {
         const profile = await usuarioModelo.findOne({ _id: req.user });
         res.status(200)
-            .json(profile)            ;
+            .json(profile);
     } catch (error) {
         console.log(error);
+    }
+});
+
+//Actualizar perfil de usuario
+router.post('/', auth, async (req, res) => {
+
+    const newValue = req.body.newValue;
+    const edit = req.body.edit;
+    const email = req.body.email;
+
+    try {
+
+        const usuarioActualizado = await usuarioModelo.findOne({ email });
+        usuarioActualizado[edit] = newValue;
+        usuarioActualizado.save();
+        res.send('Actualizado');
+    } catch (err) {
+        console.log(err);
     }
 });
 
