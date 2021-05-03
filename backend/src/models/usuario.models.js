cc78eeda8d489f071ddeda16a031b7b7fb3470c0
Julian Caceres <caceresf.julian@gmail.com>
2021-05-03 17:39:48 -0500

    Include date

--- backend/src/models/usuario.models.js
+++ backend/src/models/usuario.models.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 
 const usuarioSchema = new mongoose.Schema({
     //definimos las características de los usuarios, de la forma que serán almacenados en la db.
-    email: {type: String, required: true},
-    passwordHash: {type: String, required: true}, 
-    base64: {data: Buffer, type: String, required: true}, 
-    username: {type: String, required: true}, 
-    bio: {type: String, required: false},
-    country: {type: String, required: false}
+    email: { type: String, required: true },
+    passwordHash: { type: String, required: true },
+    base64: { data: Buffer, type: String, required: true },
+    username: { type: String, required: true },
+    bio: { type: String, required: false },
+    country: { type: String, required: false },
+    last_connection: { type: Date, required: true },
+    created: { type: Date, required: true }
 });
 
 const User = mongoose.model('usuario', usuarioSchema);
