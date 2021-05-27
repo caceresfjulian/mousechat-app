4fc2343eba5381757fd37674d1c347367c19d18f
caceresfjulian <caceresf.julian@gmail.com>
2021-05-26 22:29:35 -0500

    Remove comments, add context

--- frontend/src/components/join/Join.js
+++ frontend/src/components/join/Join.js
@@ -1,32 +1,20 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-// import io, { Socket } from 'socket.io-client'
-import './Join.css'
+import React, { useContext, useState } from 'react';
+import AuthContext from '../context/AuthContext';
+import { Link } from 'react-router-dom';
+import './Join.css';
 
-// let socket;
 
 function Join() {
 
-    const [name, setName] = useState("");
+    const { validProfile } = useContext(AuthContext);
+
+    const name = validProfile.username;
     const [room, setRoom] = useState("room1");
-    // const [users, setUsers] = useState("");
-    // const ENDPOINT = 'localhost:4000'
-
-    // useEffect(() => {
-    //     socket = io(ENDPOINT);
-
-    //     socket.on('roomData', ({users}) => {
-    //         setUsers(users)
-    //     });
-    // })
 
     return (
         <div className="joinOuterContainer">
             <div className="joinInnerContainer">
                 <h1 className="heading">Join</h1>
-                <div>
-                    <input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
-                </div>
                 <div>
                     <select placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}>
                         <option value="room1">Room 1</option>
