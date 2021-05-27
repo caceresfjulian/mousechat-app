64cc2451ff3e4c973cf0e4a907c903c6dae51ec8
caceresfjulian <caceresf.julian@gmail.com>
2021-05-27 18:16:54 -0500

    Add Link instead of a tag

--- frontend/src/components/infobar/Infobar.js
+++ frontend/src/components/infobar/Infobar.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import onlineIcon from '../../icons/onlineIcon.png';
-import closeIcon from '../../icons/closeIcon.png';
-import './Infobar.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import onlineIcon from "../../icons/onlineIcon.png";
+import closeIcon from "../../icons/closeIcon.png";
+import "./Infobar.css";
 
-function Infobar({room}) {
-    return (
-        <div className="infoBar">
-            <div className="leftInnerContainer">
-                <img src={onlineIcon} className="onlineIcon" alt="online"/>
-                <h3>{room}</h3>
-            </div>
-            <div className="rightInnerContainer">
-                <a href="/"><img src={closeIcon} alt="close"/></a>
-            </div>
-        </div>
-    )
+function Infobar({ room }) {
+  return (
+    <div className="infoBar">
+      <div className="leftInnerContainer">
+        <img src={onlineIcon} className="onlineIcon" alt="online" />
+        <h3>{room}</h3>
+      </div>
+      <div className="rightInnerContainer">
+        <Link to="/">
+          <img src={closeIcon} alt="close" />
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Infobar
+export default Infobar;
