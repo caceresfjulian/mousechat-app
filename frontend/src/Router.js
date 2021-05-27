ef13644e78725f05c9e38d4e4c2febc64d142f77
caceresfjulian <caceresf.julian@gmail.com>
2021-05-27 18:16:29 -0500

    Remove needless routes

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,52 +1,47 @@
-import React, { useContext, useState } from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import About from './components/about/About';
-import Login from './components/auth/Login';
-import Registro from './components/auth/Registro';
-import Chat from './components/chat/Chat';
-import AuthContext from './components/context/AuthContext';
-import Dashboard from './components/dashboard/Dashboard';
-import Directory from './components/directory/Directory';
-import Home from './components/home/Home';
-import Navbar from './components/layout/navbar';
-import MyProfile from './components/myProfile/MyProfile';
+import React, { useContext, useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import About from "./components/about/About";
+import Login from "./components/auth/Login";
+import Registro from "./components/auth/Registro";
+import Chat from "./components/chat/Chat";
+import AuthContext from "./components/context/AuthContext";
+import Dashboard from "./components/dashboard/Dashboard";
+import Home from "./components/home/Home";
+import Navbar from "./components/layout/navbar";
 
 //Aquí se crean las diferentes rutas del frontend gracias a react-router-dom.
 
 function Router() {
+  const { loggedIn } = useContext(AuthContext);
 
-    const { loggedIn } = useContext(AuthContext);
+  const [isOpen, setOpen] = useState(false);
 
-    const [isOpen, setOpen] = useState(false);
-
-    //Modifiqué este tipo de lógica para evitar la advertencia: <Route> elements should not change from controlled to uncontrolled "
-    if (loggedIn === true) {
-        return (
-            <BrowserRouter>
-                <Navbar isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
-                <Switch>
-                    <Route exact path="/dashboard" component={Dashboard} />
-                    <Route path="/chat" component={Chat} />
-                    <Route path="/myProfile" component={MyProfile} />
-                    <Route path="/directory" component={Directory} />
-                    <Route path="*" component={Dashboard} />
-                </Switch>
-            </BrowserRouter>)
-    }
-    else {
-        return (<BrowserRouter>
-            <Navbar isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/register" component={Registro} />
-                <Route path="/Login" component={Login} />
-                <Route path="/about" component={About} />
-                <Route path="*" component={Home} />
-            </Switch>
-        </BrowserRouter>
-        )
-    }
+  //Modifiqué este tipo de lógica para evitar la advertencia: <Route> elements should not change from controlled to uncontrolled "
+  if (loggedIn === true) {
+    return (
+      <BrowserRouter>
+        <Navbar isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
+        <Switch>
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route path="/chat" component={Chat} />
+          <Route path="*" component={Dashboard} />
+        </Switch>
+      </BrowserRouter>
+    );
+  } else {
+    return (
+      <BrowserRouter>
+        <Navbar isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/register" component={Registro} />
+          <Route path="/Login" component={Login} />
+          <Route path="/about" component={About} />
+          <Route path="*" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default Router;
-
