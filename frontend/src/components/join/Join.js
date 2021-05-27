02cf5899ccdcb38b205fa34c86e96c0e9664574d
caceresfjulian <caceresf.julian@gmail.com>
2021-05-27 00:11:16 -0500

    Add style and buttons to join box

--- frontend/src/components/join/Join.js
+++ frontend/src/components/join/Join.js
@@ -1,33 +1,61 @@
-import React, { useContext, useState } from 'react';
-import AuthContext from '../context/AuthContext';
-import { Link } from 'react-router-dom';
-import './Join.css';
-
+import React, { useContext, useState } from "react";
+import AuthContext from "../context/AuthContext";
+import { Link } from "react-router-dom";
+import "./Join.css";
 
 function Join() {
+  const { validProfile } = useContext(AuthContext);
 
-    const { validProfile } = useContext(AuthContext);
+  const name = validProfile.username;
+  const [room, setRoom] = useState("room1");
 
-    const name = validProfile.username;
-    const [room, setRoom] = useState("room1");
-
-    return (
-        <div className="joinOuterContainer">
-            <div className="joinInnerContainer">
-                <h1 className="heading">Join</h1>
-                <div>
-                    <select placeholder="Room" className="joinInput mt-20" type="text" onChange={(event) => setRoom(event.target.value)}>
-                        <option value="room1">Room 1</option>
-                        <option value="room2">Room 2</option>
-                        <option value="room3">Room 3</option>
-                    </select>
-                </div>
-                <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
-                    <button className={'button mt-20'} type="submit">Sign In</button>
-                </Link>
-            </div>
+  return (
+    <div className="join-container">
+      <div className="join-box">
+        <h1>Choose a room</h1>
+        <div>
+          <span
+            className={
+              room === "room1"
+                ? "join-box-options room-selected"
+                : "join-box-options"
+            }
+            onClick={() => setRoom("room1")}
+          >
+            Room 1
+          </span>
+          <span
+            className={
+              room === "room2"
+                ? "join-box-options room-selected"
+                : "join-box-options"
+            }
+            onClick={() => setRoom("room2")}
+          >
+            Room 2
+          </span>
+          <span
+            className={
+              room === "room3"
+                ? "join-box-options room-selected"
+                : "join-box-options"
+            }
+            onClick={() => setRoom("room3")}
+          >
+            Room 3
+          </span>
+          <Link
+            onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <button className="navigation-button py10x50 mb20" type="submit">
+              <h2 style={{ margin: 0, fontWeight: 1 }}>Join</h2>
+            </button>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
-export default Join
+export default Join;
