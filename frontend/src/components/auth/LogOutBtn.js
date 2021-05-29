52ba364333af777b31fff6bd99972d242b0f89cc
caceresfjulian <caceresf.julian@gmail.com>
2021-05-29 13:36:18 -0500

    Apply prettier and modify logout btn

--- frontend/src/components/auth/LogOutBtn.js
+++ frontend/src/components/auth/LogOutBtn.js
@@ -1,29 +1,28 @@
-import axios from 'axios'
-import React, { useContext } from 'react'
-import { useHistory } from 'react-router';
-import AuthContext from '../context/AuthContext';
+import axios from "axios";
+import React, { useContext } from "react";
+import { useHistory } from "react-router";
+import AuthContext from "../context/AuthContext";
 //Creamos botón de deslogueo que utiliza la funcion obtenerLoggeo pasada por contexto desde AuthContext
 
 function LogOutBtn() {
+  const { obtenerLoggeo, setValidProfile } = useContext(AuthContext);
 
-    const {obtenerLoggeo, setValidProfile} = useContext(AuthContext);
+  const historial = useHistory();
+  //Devuelve un array de todas las direcciones vistadas.
 
-    const historial = useHistory();
-    //Devuelve un array de todas las direcciones vistadas. 
+  async function desloggeo() {
+    await setValidProfile("");
+    await axios.get("http://localhost:4000/auth/logout");
+    await obtenerLoggeo();
+    //Para redirigir después de desloggearse, utilizamos useHistory de Router
+    historial.push("/");
+  }
 
-    async function desloggeo() {
-        await setValidProfile("");
-        await axios.get("http://localhost:4000/auth/logout");
-        await obtenerLoggeo();
-        //Para redirigir después de desloggearse, utilizamos useHistory de Router
-        historial.push("/");
-    }
-
-    return (
-        <button onClick={desloggeo}>
-            Log out
-        </button>
-    )
+  return (
+    <span onClick={desloggeo} id="logout-button">
+      Log out
+    </span>
+  );
 }
 
-export default LogOutBtn
+export default LogOutBtn;
