c61907676773bbd1e2d71f0626350957f59c626e
caceresfjulian <caceresf.julian@gmail.com>
2021-06-02 22:39:57 -0500

    Remove needless files and routes

--- backend/src/models/usuario.models.js
+++ backend/src/models/usuario.models.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 //requerimos mongoose para crear el esquema y el modelo de usuario.
 
 const usuarioSchema = new mongoose.Schema({
-    //definimos las características de los usuarios, de la forma que serán almacenados en la db.
-    email: { type: String, required: true },
-    passwordHash: { type: String, required: true },
-    base64: { data: Buffer, type: String, required: true },
-    username: { type: String, required: true },
-    bio: { type: String, required: false },
-    country: { type: String, required: false },
-    last_connection: { type: Date, required: true },
-    created: { type: Date, required: true }
+  //definimos las características de los usuarios, de la forma que serán almacenados en la db.
+  email: { type: String, required: true },
+  passwordHash: { type: String, required: true },
+  base64: { data: Buffer, type: String, required: true },
+  username: { type: String, required: true },
+  bio: { type: String, required: false },
+  country: { type: String, required: false },
+  connected: { type: Boolean, required: true },
+  created: { type: Date, required: true },
 });
 
-const User = mongoose.model('usuario', usuarioSchema);
+const User = mongoose.model("usuario", usuarioSchema);
 //creamos el modelo de usuario.
 
 module.exports = User;
