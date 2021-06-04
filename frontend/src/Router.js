8522db95363d3a604d612df6dcfdaf704a3ec094
caceresfjulian <caceresf.julian@gmail.com>
2021-06-04 12:36:34 -0500

    Add suspense and lazy for loading sites

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,13 +1,15 @@
 import React, { useContext, useState, useCallback, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import About from "./components/about/About";
-import Login from "./components/auth/Login";
-import Registro from "./components/auth/Registro";
+import LoadingPage from "./components/loadingPage/LoadingPage";
 import Chat from "./components/chat/Chat";
 import AuthContext from "./components/context/AuthContext";
 import Dashboard from "./components/dashboard/Dashboard";
-import Home from "./components/home/Home";
 import Navbar from "./components/layout/navbar";
+
+const Home = React.lazy(() => import("./components/home/Home"));
+const About = React.lazy(() => import("./components/about/About"));
+const Login = React.lazy(() => import("./components/auth/Login"));
+const Registro = React.lazy(() => import("./components/auth/Registro"));
 
 //Aquí se crean las diferentes rutas del frontend gracias a react-router-dom.
 
@@ -32,27 +34,31 @@ function Router() {
   //Modifiqué este tipo de lógica para evitar la advertencia: <Route> elements should not change from controlled to uncontrolled "
   if (loggedIn === true) {
     return (
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route path="/chat" component={Chat} />
-          <Route path="*" component={Dashboard} />
-        </Switch>
-      </BrowserRouter>
+      <React.Suspense fallback={<LoadingPage />}>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route path="/chat" component={Chat} />
+            <Route path="*" component={Dashboard} />
+          </Switch>
+        </BrowserRouter>
+      </React.Suspense>
     );
   } else {
     return (
-      <BrowserRouter>
-        <Navbar isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/register" component={Registro} />
-          <Route path="/Login" component={Login} />
-          <Route path="/about" component={About} />
-          <Route path="*" component={Home} />
-        </Switch>
-      </BrowserRouter>
+      <React.Suspense fallback={<LoadingPage />}>
+        <BrowserRouter>
+          <Navbar isOpen={isOpen} toggle={() => setOpen(!isOpen)} />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/register" component={Registro} />
+            <Route path="/Login" component={Login} />
+            <Route path="/about" component={About} />
+            <Route path="*" component={Home} />
+          </Switch>
+        </BrowserRouter>
+      </React.Suspense>
     );
   }
 }
