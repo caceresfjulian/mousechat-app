847630dfb22537f7ed55ac94c8a99692d937287a
caceresfjulian <caceresf.julian@gmail.com>
2021-06-03 21:47:25 -0500

    Add animation

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,93 +1,121 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useHistory } from 'react-router';
-import axios from 'axios';
-import swal from 'sweetalert';
-import AuthContext from '../context/AuthContext';
-import './Login.css';
-
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { useHistory } from "react-router";
+import Fade from "react-reveal/Fade";
+import axios from "axios";
+import swal from "sweetalert";
+import AuthContext from "../context/AuthContext";
+import "./Login.css";
 
 function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    //definimos los datos que serán capturados para ser enviados al back.
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  //definimos los datos que serán capturados para ser enviados al back.
 
-    const { obtenerLoggeo } = useContext(AuthContext);
-    const historial = useHistory();
+  const { obtenerLoggeo } = useContext(AuthContext);
+  const historial = useHistory();
 
-    //Definimos una función ASÍNCRONA para el onSubmit del form
-    async function loggeo(e) {
-        e.preventDefault();
-        if (email.trim().length < 8 || password.length < 8) {
-            swal('Error', 'Fill the form and try again.', 'error');
-        } else {
-            // Escribimos un try catch para enviar la solicitud al backend, almacenar la info en una const.
-            // Aquí podríamos usar fetch para enviar la info, pero es preferible axios por su simpleza.
-            try {
-                const datosLogin = {
-                    email,
-                    password
-                };
+  //Definimos una función ASÍNCRONA para el onSubmit del form
+  async function loggeo(e) {
+    e.preventDefault();
+    if (email.trim().length < 8 || password.length < 8) {
+      swal("Error", "Fill the form and try again.", "error");
+    } else {
+      // Escribimos un try catch para enviar la solicitud al backend, almacenar la info en una const.
+      // Aquí podríamos usar fetch para enviar la info, pero es preferible axios por su simpleza.
+      try {
+        const datosLogin = {
+          email,
+          password,
+        };
 
-                await axios.post("http://localhost:4000/auth/login", datosLogin);
-                //Hay que habilitar el uso de cookies, entonces en app.js añadimos withcredentials.
+        await axios.post("http://localhost:4000/auth/login", datosLogin);
+        //Hay que habilitar el uso de cookies, entonces en app.js añadimos withcredentials.
 
-                await obtenerLoggeo();
-                historial.push("/dashboard");
-                //Añadimos de nuevo esto para actualizar el contexto y redireccionar.
-
-            } catch (error) {
-                swal('Error', 'Check the provided information', 'error');
-            }
-        }
+        await obtenerLoggeo();
+        historial.push("/dashboard");
+        //Añadimos de nuevo esto para actualizar el contexto y redireccionar.
+      } catch (error) {
+        swal("Error", "Check the provided information", "error");
+      }
     }
+  }
 
-    const dummyAccount = () => {
-        setEmail('test@gmail.com');
-        setPassword('password');
-    }
+  const dummyAccount = () => {
+    setEmail("test@gmail.com");
+    setPassword("password");
+  };
 
-    //Creamos una encuesta común.
-    return (
-        //Añadimos a cada input el valor y hook asociado por onChange.
-        <div id="login-container">
-            <div id="login-title">
-                <h1 style={{ fontWeight: 1 }}>Login</h1>
-            </div>
-            <div style={{ display: 'flex ', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-                <h2 style={{ fontWeight: 1 }}>Welcome Back...</h2>
-                <div id="login-form">
-                    <form onSubmit={loggeo} style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-                        <input
-                            className="login-input"
-                            type="email"
-                            placeholder="Email address"
-                            value={email}
-                            maxLength="30"
-                            onChange={(e) => setEmail(e.target.value)}
-                            style={{ marginBottom: 20, marginTop: 40 }}
-                        />
-                        <input
-                            className="login-input"
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            maxLength="30"
-                            onChange={(e) => setPassword(e.target.value)}
-                            style={{ marginBottom: 20 }}
-                        />
-                        <p style={{ textAlign: 'center', marginTop: 15, marginBottom: 40}}>Don’t want to create an account? <br />
-                            <span style={{ color: '#FF0000', display: 'inline', cursor: 'pointer' }}
-                                onClick={() => dummyAccount()}>Login</span> with our dummy profile </p>
-                        <button type="submit">Log in</button>
-                    </form>
-                </div>
-                <p style={{ textAlign: 'center', marginTop: 10 }}>
-                    Or <Link to="/register">create</Link> an account!
-                        </p>
-            </div>
-        </div>
-    )
+  //Creamos una encuesta común.
+  return (
+    //Añadimos a cada input el valor y hook asociado por onChange.
+    <div id="login-container">
+      <div id="login-title">
+        <h1 style={{ fontWeight: 1 }}>Login</h1>
+      </div>
+      <div
+        style={{
+          display: "flex ",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h2 style={{ fontWeight: 1 }}>Welcome Back...</h2>
+        <Fade duration={2000}>
+          <div id="login-form">
+            <form
+              onSubmit={loggeo}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <input
+                className="login-input"
+                type="email"
+                placeholder="Email address"
+                value={email}
+                maxLength="30"
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ marginBottom: 20, marginTop: 40 }}
+              />
+              <input
+                className="login-input"
+                type="password"
+                placeholder="Password"
+                value={password}
+                maxLength="30"
+                onChange={(e) => setPassword(e.target.value)}
+                style={{ marginBottom: 20 }}
+              />
+              <p
+                style={{ textAlign: "center", marginTop: 15, marginBottom: 40 }}
+              >
+                Don’t want to create an account? <br />
+                <span
+                  style={{
+                    color: "#FF0000",
+                    display: "inline",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => dummyAccount()}
+                >
+                  Login
+                </span>{" "}
+                with our dummy profile{" "}
+              </p>
+              <button type="submit">Log in</button>
+            </form>
+          </div>
+          <p style={{ textAlign: "center", marginTop: 10 }}>
+            Or <Link to="/register">create</Link> an account!
+          </p>
+        </Fade>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
