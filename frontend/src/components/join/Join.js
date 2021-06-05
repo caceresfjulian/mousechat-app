54cb5fc00b1246ca71f0cab9fe08f87a919c32fd
caceresfjulian <caceresf.julian@gmail.com>
2021-06-04 22:20:35 -0500

    Remove Fade cause profile icon fade too

--- frontend/src/components/join/Join.js
+++ frontend/src/components/join/Join.js
@@ -11,53 +11,51 @@ function Join() {
   const [room, setRoom] = useState("room1");
 
   return (
-    <Fade>
-      <div className="join-container">
-        <div className="join-box">
-          <h1>Choose a room</h1>
-          <div>
-            <span
-              className={
-                room === "room1"
-                  ? "join-box-options room-selected"
-                  : "join-box-options"
-              }
-              onClick={() => setRoom("room1")}
-            >
-              Room 1
-            </span>
-            <span
-              className={
-                room === "room2"
-                  ? "join-box-options room-selected"
-                  : "join-box-options"
-              }
-              onClick={() => setRoom("room2")}
-            >
-              Room 2
-            </span>
-            <span
-              className={
-                room === "room3"
-                  ? "join-box-options room-selected"
-                  : "join-box-options"
-              }
-              onClick={() => setRoom("room3")}
-            >
-              Room 3
-            </span>
-            <Link
-              onClick={(e) => (!name || !room ? e.preventDefault() : null)}
-              to={`/chat?name=${name}&room=${room}`}
-            >
-              <button id="join-button" className="py10x50 mb20" type="submit">
-                <h2 style={{ margin: 0, fontWeight: 1 }}>Join</h2>
-              </button>
-            </Link>
-          </div>
+    <div className="join-container">
+      <div className="join-box">
+        <h1>Choose a room</h1>
+        <div>
+          <span
+            className={
+              room === "room1"
+                ? "join-box-options room-selected"
+                : "join-box-options"
+            }
+            onClick={() => setRoom("room1")}
+          >
+            Room 1
+          </span>
+          <span
+            className={
+              room === "room2"
+                ? "join-box-options room-selected"
+                : "join-box-options"
+            }
+            onClick={() => setRoom("room2")}
+          >
+            Room 2
+          </span>
+          <span
+            className={
+              room === "room3"
+                ? "join-box-options room-selected"
+                : "join-box-options"
+            }
+            onClick={() => setRoom("room3")}
+          >
+            Room 3
+          </span>
+          <Link
+            onClick={(e) => (!name || !room ? e.preventDefault() : null)}
+            to={`/chat?name=${name}&room=${room}`}
+          >
+            <button id="join-button" className="py10x50 mb20" type="submit">
+              <h2 style={{ margin: 0, fontWeight: 1 }}>Join</h2>
+            </button>
+          </Link>
         </div>
       </div>
-    </Fade>
+    </div>
   );
 }
 
