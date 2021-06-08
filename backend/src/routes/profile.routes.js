416f6a68bf7ce6f585bc89b3c7bac9da61ef86bb
caceresfjulian <caceresf.julian@gmail.com>
2021-06-08 16:32:52 -0500

    Translate comments to EN

--- backend/src/routes/profile.routes.js
+++ backend/src/routes/profile.routes.js
@@ -1,36 +1,33 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const auth = require('../middleware/auth');
+const auth = require("../middleware/auth");
 
-const usuarioModelo = require('../models/usuario.models')
+const usuarioModelo = require("../models/usuario.models");
 
-//Cargar perfil de usuario
-router.get('/', auth, async (req, res) => {
-    try {
-        const profile = await usuarioModelo.findOne({ _id: req.user });
-        res.status(200)
-            .json(profile);
-    } catch (error) {
-        console.log(error);
-    }
+//Get the user's profile info
+router.get("/", auth, async (req, res) => {
+  try {
+    const profile = await usuarioModelo.findOne({ _id: req.user });
+    res.status(200).json(profile);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-//Actualizar perfil de usuario
-router.post('/', auth, async (req, res) => {
+//Update the new profile's info
+router.post("/", auth, async (req, res) => {
+  const newValue = req.body.newValue;
+  const edit = req.body.edit;
+  const email = req.body.email;
 
-    const newValue = req.body.newValue;
-    const edit = req.body.edit;
-    const email = req.body.email;
-
-    try {
-
-        const usuarioActualizado = await usuarioModelo.findOne({ email });
-        usuarioActualizado[edit] = newValue;
-        usuarioActualizado.save();
-        res.send('Actualizado');
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const usuarioActualizado = await usuarioModelo.findOne({ email });
+    usuarioActualizado[edit] = newValue;
+    usuarioActualizado.save();
+    res.send("Actualizado");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
